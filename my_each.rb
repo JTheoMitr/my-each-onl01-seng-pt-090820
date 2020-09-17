9047b19a406522ff90f7f17513ae34675d3dc7fe
John Mitropoulos <jmitropoulos@alumni.berklee.edu>
2020-09-17 20:45:06 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -2,13 +2,13 @@ def my_each(array)
   #set our counter variable
   i = 0
  
- #only run while i is less than array length
+  #only run while i is less than array length
   while i < array.length
-  #grab value at [i]
+    #grab value at [i]
     yield(array[i])
-    increment value of [i]
+    #increment value of [i]
     i = i + 1
   end
- 
+  #return original array
   array
 end
